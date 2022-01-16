64a451c957dce32787e708a8a2d30b2c4b79a7f0
Платонова Дарья Андреевна <plat-dasha@mail.ru>
2022-01-16 11:36:30 +0300

    added styling exercise

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={styles.header}>Here are some boxes of different colors</Text>
+      </View>
+      <View style={[styles.container, styles.cyan]}>
+        <Text style={[styles.subTitle]}>Cyan: #2aa198</Text>
+      </View>
+      <View style={[styles.container, styles.blue]}>
+        <Text style={[styles.subTitle]}>Blue: #268bd2</Text>
+      </View>
+      <View style={[styles.container, styles.magenta]}>
+        <Text style={[styles.subTitle]}>Magenta: #d33682</Text>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 30,
     alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: 10,
+    marginBottom: 10,
   },
-});
+  cyan: { backgroundColor: '#2aa198' },
+  blue: { backgroundColor: '#268bd2' },
+  magenta: { backgroundColor: '#d33682' },
+  subTitle: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 14,
+  },
+  header: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  }
+})
+
+export default App;
